5297e34332a34c9738e030eb47c98c2139911927
niko <ndrei.mobil@googlemail.com>
2017-04-07 23:10:51 +0200

    add standards about game actions + Menu events add standards about device stats

--- code/modules/standards.js
+++ code/modules/standards.js
@@ -1,16 +1,45 @@
 //This File holds global enum like values
 
-//ALL DISPLAY-TYPES IN HERE
-exports.DISPLAY_TYPE = Object.freeze( {
-		MOCK:"DISPLAY_MOCK", //default Type of Display for testing purpose
-		SSD1306I2C:"DISPLAY_SSD1306I2C" // first Display wich runs with piLazOrTag
-	});
 //ALL Possible Actions for the EventHandling
 exports.ACTIONS = Object.freeze({
+        //maybe fire is due non blocking decisons, done seperatly
         FIRE: 1,
+        
+        //receive a Hit from another player
         GETHIT: 2,
-        //================= 
-        MENUE: 98,
-        INIT: 99,
-        SYNCTIME: 100
+        
+        //receive ammo
+        GETAMMO:3,
+
+        //receive health
+        GETHEALTH:4,
+
+        //take the flag
+        GETFLAG:5,
+
+        //set the starttime
+        GETSTARTTIME:6
+
+        //MENU 
+        MENUEOPEN: 95,
+        MENUEUP: 96,
+        MENUDOWN: 97,
+        MENUENTER: 98,
+
+        //receive
+        SYNCTIME: 99
+        INIT: 100,
+        
     });
+
+//Register all  DISPLAY-TYPES here
+exports.DISPLAY_TYPE = Object.freeze( {
+        MOCK:"DISPLAY_MOCK", //default Type of Display for testing purpose
+        SSD1306I2C:"DISPLAY_SSD1306I2C" // first Display wich runs with piLazOrTag
+    });
+//All Device States
+exports.STATES = Object.freeze({
+        MASTER: "MASTER",
+        SLAVE: "SLAVE",
+        GUN: "GUN"
+});
