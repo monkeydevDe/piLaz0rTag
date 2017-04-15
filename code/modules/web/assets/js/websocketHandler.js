ffa0975f0b8442b0137cd33bdd245315bad52991
tuxBurner <tuxBurner@boggo.de>
2017-04-15 14:30:59 +0200

    #6 Better web view for the data

--- code/modules/web/assets/js/websocketHandler.js
+++ code/modules/web/assets/js/websocketHandler.js
@@ -7,15 +7,18 @@ $(function() {
     magsAvaible: ko.observable(0),
     nrOfMags: ko.observable(0),
     health: ko.observable(0),
-    totalHealth: ko.observable(0)
+    totalHealth: ko.observable(0),
+    playerId: ko.observable(0),
+    team: ko.observable(''),
+    shootStrength: ko.observable(0)
   };
   ko.applyBindings(PlayerViewModel);
 
 
   var socket = io();
 
+  // bind click event to buttons to emit data over the websocket
   $('.socketMsg').on('click', function() {
-
     var msgType = $(this).data('messageType');
     var msgVal = $(this).data('messageValue');
     socket.emit('socketMessage', {"type" : msgType, "value" : msgVal});
@@ -23,13 +26,16 @@ $(function() {
 
   // register event to update display
   socket.on('display', function(data) {
-    console.error(data);
-
+    
+    // check if to enable disable certain buttons
     var enableReloadBtn = data.status.reloading;
     $('#reloadBtn').prop("disabled", enableReloadBtn);
     $('#shootBtn').prop("disabled", enableReloadBtn);
 
 
+    PlayerViewModel.playerId(data.id);
+    PlayerViewModel.shootStrength(data.shootStrength);
+    PlayerViewModel.team(data.team);
     PlayerViewModel.rounds(data.status.roundsInMag);
     PlayerViewModel.roundsPerMag(data.roundsPerMag);
     PlayerViewModel.magsAvaible(data.status.mags);
