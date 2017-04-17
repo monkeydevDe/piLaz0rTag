a2d06137cda3f426fdd6a359ac424e95c09a1773
niko <ndrei.mobil@googlemail.com>
2017-04-17 23:03:09 +0200

    add a ir mock

--- code/modules/infrared/InfraredFactory.js
+++ code/modules/infrared/InfraredFactory.js
@@ -9,11 +9,17 @@ class InfraredFactory {
 
     const settings = require('../settings.js');
     const standards = require('../standards.js');
-
+ 
     if(settings.INFRARED === standards.INFRARED_TYPE.LIRCD) {
       log.info("Infrared: LIRCD infrared is configured.");
       const { LircdInfraredHandler } = require('./LircdInfraredHandler.js');
       return new LircdInfraredHandler();
+    }
+
+    if(settings.INFRARED === standards.INFRARED_TYPE.IRMOCK) {
+      log.info("Infrared: MOCK is configured.");
+      const { IrMockHandler } = require('./IrMockHandler.js');
+      return new IrMockHandler();
     }
 
     throw new Error('No infrared handler configured!');
