e43216b303d1d3fc72cf09353f68376e2a139182
Sebastian Hardt <tuxburner@boggo.de>
2017-06-10 15:32:41 +0200

    #8 Unique id is now better

--- code/modules/bonjour/BonjourHandler.js
+++ code/modules/bonjour/BonjourHandler.js
@@ -6,13 +6,14 @@ class BonjourHandler extends BaseClass {
     this.bonjour = require('bonjour')();
 
     const os = require('os');
-    this.uniqueId = os.hostname();
+    this.masterServiceName = 'pilazorTag_master_'+os.hostname();
 
     this.mastetService = null;
 
     const instance = this;
 
     this.browser = this.bonjour.find({type: 'piLaz0rTag'},function(service) {
+      console.error(service);
       const value = service.addresses[0];
       instance.log.info('Bonjour: Got service for master: '+value);
       instance.eventHandler.mainEvents.UPDATE_STATE_DATA.emit({type: 'bonjour',value: value});
@@ -25,8 +26,8 @@ class BonjourHandler extends BaseClass {
    * Cast the master Host when in master mode
    */
   publishMasterHost() {
-    this.log.info('Bonjour: Casting Master mode server on port: '+this.settings.WEBSERVER_PORT);
-    this.mastetService  = this.bonjour.publish({ name: 'pilazorTag_master_'+this.uniqueId, type: 'piLaz0rTag', port: this.settings.WEBSERVER_PORT })
+    this.log.info('Bonjour: Casting Master mode server on port: '+this.settings.WEBSERVER_PORT+' name: '+this.masterServiceName);
+    this.mastetService  = this.bonjour.publish({ name: this.masterServiceName, type: 'piLaz0rTag', port: this.settings.WEBSERVER_PORT })
   }
 
 
